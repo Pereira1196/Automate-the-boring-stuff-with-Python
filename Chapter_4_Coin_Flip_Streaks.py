f4a5f5120f09c8137a4dd9d9e3a43aa33a88bfe4
Douglas Silva <douglas.pereira1196@gmail.com>
2023-06-04 11:39:51 -0300

    Incluindo o exercicio da moeda

---
+++ Chapter_4_Coin_Flip_Streaks.py
@@ -0,0 +1,26 @@
+"""
+Programa para testar o aprendizado de listas
+Autor: Douglas Silva
+Data: 04/06/2023
+"""
+
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    listOfNumbers = []
+    for throw in range(100):
+        listOfNumbers.append(random.randint(0,1))
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    streaks = 0
+    for index, item in enumerate(listOfNumbers):
+        if streaks == 5:
+            numberOfStreaks += 1
+            streaks = 0
+        elif index == len(listOfNumbers) - 1:
+            break
+        elif item == listOfNumbers[index + 1]:
+            streaks += 1
+        else:
+            streaks = 0
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
